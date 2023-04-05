8aeedad810e8a069e334f362862036ea2bfaa6a1
veronicaKMS <kms.veronica@javarndventures.com>
2023-04-06 01:40:23 +0530

    f/task-menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [message, setMessage] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1>Task list</h1>
+      <input
+        type="email"
+        value={message}
+        onChange={event => setMessage(event.target.value)}
+      />
 
-export default App;
+      <button disabled={!message}>Submit</button>
+    </div>
+  )
+}
